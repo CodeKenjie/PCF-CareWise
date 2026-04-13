c1462d2c4a5c847861d7e5163b9fd57679c97478
kenjie <franciskenjiexd@gmail.com>
2026-04-13 13:12:42 +0800

    ...

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,35 +1,48 @@
-<?php require __DIR__ . '/../partials/header.php' ?>
-<main id="login">
-    <a class="redirect p-md" href="/register">Register</a>
-    <section>
-        <header>
-            <img class="logo" src="/assets/images/logo.png" alt="">
-            <h2>PCF:CareWise</h2>
-        </header>
-        <form class="form-ui" action="/login" method="post">
-            <span class="p-sm">
-                <h1 class="ctr-text spr-h">Hi Nurse</h1>
-                <h2 class="ctr-text sb-h">Welcome to CareWise</h2>
-            </span>
-            <span class="p-sm">
-                <input id="email" name="email" type="email" maxlength="150" required>
-                <label for="email">Email</label>
-            </span>
-            <span class="p-sm">
-                <div class="password">
-                    <input id="password" name="password" id="password" type="password" required>
-                    <button type="button" onclick="view(`viewPass`, `password`)">
-                        <img id="viewPass" class="icon" src="/assets/images/hide.svg" alt="view">
-                    </button>
-                </div>
-                <label for="password">Password</label>
-            </span>
-            <a class="forgotten" href="#">Forgotten?</a>
-            <input type="submit" value="Login">
-        </form>
-    </section>
-    <div class="site">
-        <img src="assets/images/site.jpg" alt="">
-    </div>
-</main>
-<?php require __DIR__ . '/../partials/footer.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="icon" href="assets/images/logo.png"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <link rel="stylesheet" href="assets/css/media.css"/>
+</head>
+<body>
+    <div id="responseContainer"></div>
+    <main id="login">
+        <a class="redirect p-md" href="/register">Register</a>
+        <section>
+            <header>
+                <img class="logo" src="/assets/images/logo.png" alt="">
+                <h2>PCF:CareWise</h2>
+            </header>
+            <form class="form-ui" action="/login" method="post">
+                <span class="p-sm">
+                    <h1 class="ctr-text spr-h">Hi Nurse</h1>
+                    <h2 class="ctr-text sb-h">Welcome to CareWise</h2>
+                </span>
+                <span class="p-sm">
+                    <input id="email" name="email" type="email" maxlength="150" required>
+                    <label for="email">Email</label>
+                </span>
+                <span class="p-sm">
+                    <div class="password">
+                        <input id="password" name="password" id="password" type="password" required>
+                        <button type="button" onclick="view(`viewPass`, `password`)">
+                            <img id="viewPass" class="icon" src="/assets/images/hide.svg" alt="view">
+                        </button>
+                    </div>
+                    <label for="password">Password</label>
+                </span>
+                <a class="forgotten" href="#">Forgotten?</a>
+                <input type="submit" value="Login">
+            </form>
+        </section>
+        <div class="site">
+            <img src="assets/images/site.jpg" alt="">
+        </div>
+    </main>
+    <script src="assets/js/script.js" defer></script>
+</body>
+</html>
--- app/Views/pages/register.php
+++ app/Views/pages/register.php
@@ -1,80 +1,93 @@
-<?php require __DIR__ . "/../partials/header.php" ?>
-<main id="register">
-    <a class="redirect p-md" href="/login">Login</a>
-    <div class="site">
-        <img src="assets/images/site.jpg" alt="">
-    </div>
-    <section>
-        <header>
-            <img class="logo" src="/assets/images/logo.png" alt="">
-            <h2>PCF:CareWise</h2>
-        </header>
-        <form class="form-ui" action="/register" method="post">
-            <span class="p-sm">
-                <h1 class="ctr-text spr-h">Join our team</h1>
-                <h2 class="ctr-text sb-h">help our community</h2>
-            </span>
-            <span class="p-sm">
-                <input id="displayName" name="displayName" type="text" maxlength="50" required>
-                <label for="displayName">Display Name</label>
-            </span>
-            <div class="part p-sm">
-                <span>
-                    <input id="firstName" name="firstName" type="text" maxlength="100" required>
-                    <label for="firstName">First Name</label>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="icon" href="assets/images/logo.png"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <link rel="stylesheet" href="assets/css/media.css"/>
+</head>
+<body>
+    <div id="responseContainer"></div>
+    <main id="register">
+        <a class="redirect p-md" href="/login">Login</a>
+        <div class="site">
+            <img src="assets/images/site.jpg" alt="">
+        </div>
+        <section>
+            <header>
+                <img class="logo" src="/assets/images/logo.png" alt="">
+                <h2>PCF:CareWise</h2>
+            </header>
+            <form class="form-ui" action="/register" method="post">
+                <span class="p-sm">
+                    <h1 class="ctr-text spr-h">Join our team</h1>
+                    <h2 class="ctr-text sb-h">help our community</h2>
                 </span>
-                <span>
-                    <input id="lastName" name="lastName" type="text" maxlength="100" required>
-                    <label for="lastName">Last Name</label>
+                <span class="p-sm">
+                    <input id="displayName" name="displayName" type="text" maxlength="50" required>
+                    <label for="displayName">Display Name</label>
                 </span>
-            </div>
-            <div class="part p-sm">
-                <span>
-                    <select name="sex" required>
-                        <option value="" hidden></option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                    </select>
-                    <label for="sex">Sex</label>
-                </span>
-                <span>
-                    <select name="role" required>
-                        <option value="" hidden></option>
-                        <option value="admin">Admin</option>
-                        <option value="staff">Staff</option>
-                    </select>
-                    <label for="role">Role</label>
-                </span>
-            </div>
-            <span class="p-sm">
-                <input id="email" name="email" type="email" maxlength="150" required>
-                <label for="email">Email</label>
-            </span>
-            <span class="p-sm">
-                <div class="password">
-                    <input id="password" name="password" id="password" type="password" required>
-                    <button type="button" onclick="view(`viewPass`, `password`)">
-                        <img id="viewPass" class="icon" src="/assets/images/hide.svg" alt="view">
-                    </button>
+                <div class="part p-sm">
+                    <span>
+                        <input id="firstName" name="firstName" type="text" maxlength="100" required>
+                        <label for="firstName">First Name</label>
+                    </span>
+                    <span>
+                        <input id="lastName" name="lastName" type="text" maxlength="100" required>
+                        <label for="lastName">Last Name</label>
+                    </span>
                 </div>
-                <label for="password">Password</label>
-            </span>
-            <h4 id="strength" class="p-sm"></h4>
-            <span class="p-sm">
-                <div class="password">
-                    <input id="confPass" name="confPass" id="confPass" type="password" required>
-                    <button type="button" onclick="view(`viewConfPass`, `confPass`)">
-                        <img id="viewConfPass" class="icon" src="/assets/images/hide.svg" alt="view">
-                    </button>
+                <div class="part p-sm">
+                    <span>
+                        <select name="sex" required>
+                            <option value="" hidden></option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                        <label for="sex">Sex</label>
+                    </span>
+                    <span>
+                        <select name="role" required>
+                            <option value="" hidden></option>
+                            <option value="admin">Admin</option>
+                            <option value="staff">Staff</option>
+                        </select>
+                        <label for="role">Role</label>
+                    </span>
                 </div>
-                <label for="confPass">Confirm Password</label>
-            </span>
-            <div class="p-md">
-                <input name="accept" type="checkbox" required>
-                <label for="accept">Accept the <a href="">terms and condition</a></label>
-            </div>
-            <input type="submit" value="Register Account">
-        </form>
-    </section>
-</main>
-<?php require __DIR__ . "/../partials/footer.php" ?>
+                <span class="p-sm">
+                    <input id="email" name="email" type="email" maxlength="150" required>
+                    <label for="email">Email</label>
+                </span>
+                <span class="p-sm">
+                    <div class="password">
+                        <input id="password" name="password" id="password" type="password" required>
+                        <button type="button" onclick="view(`viewPass`, `password`)">
+                            <img id="viewPass" class="icon" src="/assets/images/hide.svg" alt="view">
+                        </button>
+                    </div>
+                    <label for="password">Password</label>
+                </span>
+                <h4 id="strength" class="p-sm"></h4>
+                <span class="p-sm">
+                    <div class="password">
+                        <input id="confPass" name="confPass" id="confPass" type="password" required>
+                        <button type="button" onclick="view(`viewConfPass`, `confPass`)">
+                            <img id="viewConfPass" class="icon" src="/assets/images/hide.svg" alt="view">
+                        </button>
+                    </div>
+                    <label for="confPass">Confirm Password</label>
+                </span>
+                <div class="p-md">
+                    <input name="accept" type="checkbox" required>
+                    <label for="accept">Accept the <a href="">terms and condition</a></label>
+                </div>
+                <input type="submit" value="Register Account">
+            </form>
+        </section>
+    </main>
+    <script src="assets/js/script.js" defer></script>
+</body>
+</html>
